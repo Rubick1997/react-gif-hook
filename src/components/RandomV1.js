ec4e3d36cf6326413012073a5c4465f9b467ea98
Rustam <rustam905@gmail.com>
2021-05-12 13:43:49 -0700

    main functionality implemented

--- src/components/RandomV1.js
+++ src/components/RandomV1.js
@@ -3,19 +3,29 @@ import axios from "axios";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 function RandomV1() {
+	const [gif, setGif] = useState("");
+
+	const fetchGif = async () => {
+		const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=&rating=g`;
+		const { data } = await axios.get(url);
+		const imageSrc = data.data.images.downsized_large.url;
+		setGif(imageSrc);
+	};
+
 	useEffect(() => {
-		const fetchGif = async () => {
-			const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=&rating=g`;
-			const {data} = await axios.get(url);
-            console.log(data)
-		};
-        fetchGif()
+		fetchGif();
 	}, []);
 
+	const handleClick = () => {
+		fetchGif();
+	};
+
 	return (
-		<>
-			<h1>Random</h1>
-		</>
+		<div className ="container">
+			<h1>Random Gif </h1>
+			<img src={gif} alt='Random Gif' width='500' />
+			<button onClick={handleClick}>New Gif</button>
+		</div>
 	);
 }
 
--- src/components/TagV1.js
+++ src/components/TagV1.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 function TagV1() {
-    return (
-        <>
-            <h1>Tag</h1>
-        </>
-    )
+	const [tag, setTag] = useState("dogs");
+	const [gif, setGif] = useState("");
+
+	const fetchGif = async (tag) => {
+		const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${tag}`;
+		const { data } = await axios.get(url);
+		const imageSrc = data.data.images.downsized_large.url;
+		setGif(imageSrc);
+	};
+
+	useEffect(() => {
+		fetchGif(tag);
+	}, [tag]);
+
+	const handleClick = () => {
+		fetchGif(tag);
+	};
+
+	return (
+		<div className='container'>
+			<h1>Random {tag} Gif </h1>
+			<img src={gif} alt='Random Gif' width='500' />
+			<input
+				value={tag}
+				onChange={(e) => {
+					setTag(e.target.value);
+				}}
+			/>
+			<button onClick={handleClick}>New Gif</button>
+		</div>
+	);
 }
 
-export default TagV1
+export default TagV1;
