49a58dd74694fd944e0fc005c8cd073b50595e8b
Rustam <rustam905@gmail.com>
2021-05-12 13:10:11 -0700

    request configure

--- src/components/RandomV1.js
+++ src/components/RandomV1.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import {API_KEY} from "../apiKey"
 
-const API_KEY = process.env.REACT_APP_API_KEY;
 
 function RandomV1() {
-    return (
-        <>
-            <h1>Random</h1>
-        </>
-    )
+	useEffect(() => {
+		const fetchGif = async () => {
+			const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=&rating=g`;
+			const {data} = await axios.get(url);
+            console.log(data)
+		};
+        fetchGif()
+	}, []);
+
+	return (
+		<>
+			<h1>Random</h1>
+		</>
+	);
 }
 
-export default RandomV1
+export default RandomV1;
