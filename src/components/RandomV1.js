f38ef73c34aeee88b53924df78a20ceaf6626c57
Rustam <rustam905@gmail.com>
2021-05-12 12:46:00 -0700

    Api key

---
+++ src/components/RandomV1.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+function RandomV1() {
+    return (
+        <>
+            <h1>Random</h1>
+        </>
+    )
+}
+
+export default RandomV1
---
+++ src/components/TagV1.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TagV1() {
+    return (
+        <>
+            <h1>Tag</h1>
+        </>
+    )
+}
+
+export default TagV1
